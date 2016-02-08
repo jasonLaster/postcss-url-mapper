87267824c37f0907291981582e4b875105fad520
Igor Adamenko <mail@igoradamenko.com>
2016-02-08 11:04:30 +0300

    add module

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-url-mapper', function(map, options) {
+	return function(css) {
+		if (typeof map != 'function') {
+			return;
+		}
+		
+		options = options || {
+			atRules: false
+		};
+
+		var	declsRegex = /^(?=cue|play|background|content|src|cursor)/,
+			urlRegex = /url\(\s*['"]?(?!['"]?data:)(.*?)['"]?\s*\)/g;
+
+		css.walkDecls(declsRegex, function(decl) {
+			decl.value = decl.value.replace(urlRegex, function(match, url) {
+				return 'url("' + map(url, decl.prop) + '")';
+			});
+		});
+
+		if (options.atRules) {
+			css.walkAtRules('import', function(rule) {
+				rule.params = rule.params.replace(urlRegex, function(match, url) {
+					return 'url("' + map(url, rule.name) + '")';
+				});
+			});
+		}
+	};
+});
