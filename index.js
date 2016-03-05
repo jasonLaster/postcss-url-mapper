0151e53aa74b64e629922cbd11513dd7d1896b05
Igor Adamenko <mail@igoradamenko.com>
2016-03-06 01:30:07 +0300

    add linter

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 module.exports = require('postcss').plugin('postcss-url-mapper', function(map, options) {
+	var replacer = function(value, name) {
+		return value.replace(/url\(\s*['"]?(?!['"]?data:)(.*?)['"]?\s*\)/g, function(match, url) {
+			return 'url("' + map(url, name) + '")';
+		});
+	};
+
+	options = options || {
+		atRules: false
+	};
+
 	return function(css) {
-		if (typeof map != 'function') {
+		if (typeof map !== 'function') {
 			return;
 		}
-		
-		options = options || {
-			atRules: false
-		};
-
-		var replacer = function(value, name) {
-			return value.replace(/url\(\s*['"]?(?!['"]?data:)(.*?)['"]?\s*\)/g, function(match, url) {
-				return 'url("' + map(url, name) + '")';
-			});
-		};
 
 		css.walkDecls(/^(?=cue|play|background|content|src|cursor|list-style)/, function(decl) {
 			decl.value = replacer(decl.value, decl.prop);
